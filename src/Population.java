2f8156f6a01a5dfa6bcbf3dea32911aa07327b3c
Paulo <paulozuniga17@gmail.com>
2018-09-11 16:59:01 -0600

    Created 2 classes

---
+++ src/Population.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Population {
+
+    private ArrayList<Individual> population;
+
+    public ArrayList<Individual> getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(ArrayList<Individual> population) {
+        this.population = population;
+    }
+
+    public void reproduce (Individual pIndividual1, Individual pIndividual2){
+
+        return;
+    }
+}

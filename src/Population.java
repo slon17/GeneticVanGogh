1ce633055808b72f2d1d7c93c238c9b28c9929ee
Paulo <paulozuniga17@gmail.com>
2018-09-11 21:40:08 -0600

    Began the basics of genetic algorithm

--- src/Population.java
+++ src/Population.java
@@ -1,18 +1,11 @@
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Population {
 
     private ArrayList<Individual> population;
-    private int individualsQuantity;
 
-    public Population(int individualsQuantity) {
+    public Population() {
         population = new ArrayList<>();
-        this.individualsQuantity = individualsQuantity;
     }
 
     public void generatePopulation(int initialPopulation){
@@ -23,88 +16,21 @@ public class Population {
         }
     }
 
-    public Color mutation(ArrayList<Color> mainColors){
-        Random random = new Random(System.currentTimeMillis());
-        int randomPercentage = random.nextInt(256);
-        if(randomPercentage < 20){
-            int randomColor = random.nextInt(mainColors.size());
-            return mainColors.get(randomColor);
-        }
-        return null;
-    }
-
-    public Individual reproduce(Individual individual1, Individual individual2, ArrayList<Color> mainColors) {
-        /*ArrayList <Individual> sons = new ArrayList<>();
+    public ArrayList<Individual> reproduce(){
+        ArrayList <Individual> sons = new ArrayList<>();
         ArrayList <Individual> part1 = getPartPopulation(0, population.size()/4);
         ArrayList <Individual> part2 = getPartPopulation(population.size()/4, population.size()/4*2);
         ArrayList <Individual> part3 = getPartPopulation(population.size()/4*2, population.size()/4*3);
         ArrayList <Individual> part4 = getPartPopulation(population.size()/4*3, population.size());
 
         int moduleWidth = population.get(0).getWidth()%4;
-        int moduleHeight = population.get(0).getHeight()%4;*/
+        int moduleHeight = population.get(0).getHeight()%4;
 
-        Random random = new Random(System.currentTimeMillis());
-        int randomWidth = random.nextInt(individual1.getWidth());
-        int randomHeight = random.nextInt(individual1.getHeight());
-        int randomSide = random.nextInt(2);
-        System.out.println(randomSide);
-
-        BufferedImage newImage = new BufferedImage(individual1.getWidth(), individual1.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        Color mutationColor = mutation(mainColors);
-
-        for (int i = 0; i < individual1.getWidth(); i++) {
-            for (int j = 0; j < individual1.getHeight(); j++) {
-                if (mutationColor != null) {//mutation
-                    newImage.setRGB(i, j, mutationColor.getRGB());
-                    continue;
-                }
-                if (randomSide == 0) {
-                    if (i < randomWidth && j < randomHeight) {
-                        Color newColor = new Color(individual1.getImage().getRGB(i, j));
-                        newImage.setRGB(i, j, newColor.getRGB());
-                    }
-                    else {
-                        Color newColor = new Color(individual2.getImage().getRGB(i, j));
-                        newImage.setRGB(i, j, newColor.getRGB());
-                    }
-                }
-                else {
-                    if (i < randomWidth && j < randomHeight) {
-                        Color newColor = new Color(individual2.getImage().getRGB(i, j));
-                        newImage.setRGB(i, j, newColor.getRGB());
-                    }
-                    else {
-                        Color newColor = new Color(individual1.getImage().getRGB(i, j));
-                        newImage.setRGB(i, j, newColor.getRGB());
-                    }
-                }
-            }
+        for(int i = 0; i<population.size(); i++){
+            i = i+1;
         }
-        Individual newIndividual = new Individual(newImage);
-        return newIndividual;
+        return sons;
     }
-
-    public void reproduceAll(ArrayList<Color> mainColors){
-        ArrayList<Individual> sons = new ArrayList<>();
-        ArrayList<Individual> usedParents = new ArrayList<>();
-        Random random = new Random(System.currentTimeMillis());
-        while(population.size()>1){
-            int randomParent1 = random.nextInt(population.size());
-            int randomParent2 = random.nextInt(population.size());
-            Individual parent1 = population.remove(randomParent1);
-            Individual parent2 = population.remove(randomParent2);
-            Individual son = reproduce(parent1, parent2, mainColors);
-            sons.add(son);
-            usedParents.add(parent1);
-            usedParents.add(parent2);
-        }
-        population = sons;
-        while(population.size()<individualsQuantity){
-            int randomParent = random.nextInt(usedParents.size());
-            population.add(usedParents.remove(randomParent));
-        }
-    }
-
 
     public ArrayList<Individual> getPartPopulation(int begin, int end){
         ArrayList<Individual> partPopulation = new ArrayList<>();
@@ -122,4 +48,8 @@ public class Population {
         this.population = population;
     }
 
+    public void reproduce (Individual individual1, Individual individual2){
+
+        return;
+    }
 }
